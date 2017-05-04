2702856e5d8ad8f1f680d00420a36253ee8da4c5
Lucien <lulu.senaneuch@gmail.com>
2017-05-04 10:59:26 +0200

    Update serial.h

--- src/serial.h
+++ src/serial.h
@@ -35,10 +35,24 @@
 
 #define serialPort "/dev/ttyS0"
 
+
+/*
+  def : Tente d'ouvrir un port série sur le path indiqué.
+  Return 0 si tout le port a été ouvert. Return -1 sinon.
+ */
 int robotOpenCom(const char * path=serialPort);
-int readRobot(void);
+
+/*def : Ferme le port série ouvert*/
 int robotCloseCom(void);
+
+/*def : Envoi une association commande + argument au robot.
+Celui-ci retourne 0 si la commande est OK. retourne -3 en cas de timeout. -4 en cas d'erreur de checksum
+-1 et -2 en cas d'erreur de commande et d'argument.
+Si la commande est un état de batterie la fonction retourne 0 1 ou 2 en fonction de son etat.*/
 int robotCmd(char cmd, char * arg=NULL);
+
+/*Envoi une commande sans se soucier de la réponse.
+Calcul le checksum*/
 int sendCmd(char cmd, char * arg=NULL);
 
 #endif //DUMBERC_SERIAL_H_H
